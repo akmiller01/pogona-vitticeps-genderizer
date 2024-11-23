d2a9d98c201443293885bc8a9aa1b18ce16bd3e3
akmiller01 <alex.k.miller@gmail.com>
2024-11-23 15:46:32 -0500

    Tensorboard

--- code/train_image_classifier.py
+++ code/train_image_classifier.py
@@ -8,22 +8,22 @@ import numpy as np
 from transformers import AutoImageProcessor, AutoModelForImageClassification, DefaultDataCollator, TrainingArguments, Trainer
 
 
-checkpoint = "google/vit-base-patch16-224-in21k"
+checkpoint = 'google/vit-base-patch16-224-in21k'
 image_processor = AutoImageProcessor.from_pretrained(checkpoint)
 normalize = Normalize(mean=image_processor.image_mean, std=image_processor.image_std)
 size = (
-    image_processor.size["shortest_edge"]
-    if "shortest_edge" in image_processor.size
-    else (image_processor.size["height"], image_processor.size["width"])
+    image_processor.size['shortest_edge']
+    if 'shortest_edge' in image_processor.size
+    else (image_processor.size['height'], image_processor.size['width'])
 )
 _transforms = Compose([RandomResizedCrop(size), ToTensor(), normalize])
 def transforms(examples):
-    examples["pixel_values"] = [_transforms(img.convert("RGB")) for img in examples["image"]]
-    del examples["image"]
+    examples['pixel_values'] = [_transforms(img.convert('RGB')) for img in examples['image']]
+    del examples['image']
     return examples
 
 
-accuracy = evaluate.load("accuracy")
+accuracy = evaluate.load('accuracy')
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     predictions = np.argmax(predictions, axis=1)
@@ -33,7 +33,7 @@ def compute_metrics(eval_pred):
 def main():
     dataset = load_dataset('alex-miller/pogona-vitticeps-gender')
 
-    labels = dataset["train"].features["label"].names
+    labels = dataset['train'].features['label'].names
     label2id, id2label = dict(), dict()
     for i, label in enumerate(labels):
         label2id[label] = str(i)
@@ -50,28 +50,29 @@ def main():
     )
 
     training_args = TrainingArguments(
-        output_dir="pogona-vitticeps-gender",
+        output_dir='pogona-vitticeps-gender',
         remove_unused_columns=False,
-        eval_strategy="epoch",
-        save_strategy="epoch",
-        learning_rate=5e-5,
+        eval_strategy='epoch',
+        save_strategy='epoch',
+        logging_strategy='epoch',
+        learning_rate=2e-5,
         per_device_train_batch_size=16,
         gradient_accumulation_steps=4,
         per_device_eval_batch_size=16,
-        num_train_epochs=3,
+        num_train_epochs=20,
         warmup_ratio=0.1,
-        logging_steps=10,
         load_best_model_at_end=True,
-        metric_for_best_model="accuracy",
+        metric_for_best_model='accuracy',
         push_to_hub=True,
+        report_to='tensorboard'
     )
 
     trainer = Trainer(
         model=model,
         args=training_args,
         data_collator=data_collator,
-        train_dataset=dataset["train"],
-        eval_dataset=dataset["test"],
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test'],
         processing_class=image_processor,
         compute_metrics=compute_metrics,
     )
